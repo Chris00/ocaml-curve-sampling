45c0934d052d76419db6a685d28b557dc139cef3
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2017-12-09 19:45:01 +0100

    PQ: slightly change the datastructure

--- src/PQ.ml
+++ src/PQ.ml
@@ -20,62 +20,68 @@
  *)
 
 
-type 'a t =
-| Empty
-| Heap of float * 'a * 'a t list
-(* Heap(priority, el, sub_heaps) *)
+type 'a tree = Heap of float * 'a * 'a tree list
+(* Heap(priority, el, sub_heaps with lower or equal priorities) *)
 
-let empty = Empty
+type 'a t = 'a tree option (* None: empty heap *)
 
-let is_empty q = (q = Empty) [@@inline]
+let empty = None
+
+let is_empty q = (q = None) [@@inline]
 
 let max = function
-  | Empty -> failwith "Curve_Sampling.PQ.max: empty"
-  | Heap(_, x, _) -> x
+  | None -> failwith "Curve_Sampling.PQ.max: empty"
+  | Some(Heap(_, x, _)) -> x
 
 let max_priority = function
-  | Empty -> neg_infinity
-  | Heap(p, _, _) -> p
+  | None -> neg_infinity
+  | Some(Heap(p, _, _)) -> p
 
-let[@inline] merge q1 q2 =
-  match q1, q2 with
-  | Empty, _ -> q2
-  | _, Empty -> q1
-  | Heap(p1, e1, h1), Heap(p2, e2, h2) ->
-    if p1 > p2 then Heap(p1, e1, q2 :: h1)
-    else Heap(p2, e2, q1 :: h2)
+let[@inline] merge_tree (Heap(p1, e1, h1) as t1) (Heap(p2, e2, h2) as t2) =
+  if p1 > p2 then Heap(p1, e1, t2 :: h1)
+  else Heap(p2, e2, t1 :: h2)
 
 let add q p x =
   if Gg.Float.is_nan p then
     invalid_arg "Curve_Sampling.PQ: NaN priority not allowed";
-  merge (Heap(p, x, [])) q
+  Some(match q with
+       | None -> Heap(p, x, [])
+       | Some t -> merge_tree (Heap(p, x, [])) t)
 
-let rec merge_pairs = function
-  | [] -> Empty
+(* [merge_pairs_trees l] merges a NON-EMPTY list of trees. *)
+let rec merge_pairs_trees = function
   | [h] -> h
-  | h1 :: h2 :: tl -> merge (merge h1 h2) (merge_pairs tl)
+  | h1 :: h2 :: tl -> merge_tree (merge_tree h1 h2) (merge_pairs_trees tl)
+  | [] -> assert false
+
+let[@inline] merge_pairs = function
+  | [] -> None
+  | l -> Some(merge_pairs_trees l)
 
 let delete_max = function
-  | Empty -> failwith "Curve_Sampling.PQ.delete_max: empty"
-  | Heap(_, x, []) -> (Empty, x)
-  | Heap(_, x, [h]) -> (h, x)
-  | Heap(_, x, hs) -> (merge_pairs hs, x)
+  | None -> failwith "Curve_Sampling.PQ.delete_max: empty"
+  | Some(Heap(_, x, hs)) -> (merge_pairs hs, x)
+
+let rec iter_trees f (Heap(_, x, hs)) =
+  f x;
+  List.iter (iter_trees f) hs
+
+let iter q f = match q with
+  | None -> ()
+  | Some t -> iter_trees f t
 
 
-let rec iter_ph f = function
-  | Empty -> ()
-  | Heap(_, x, hs) -> f x;  List.iter (fun h -> iter_ph f h) hs
+let rec fold_trees f init (Heap(_, x, hs)) =
+  let init = f init x in
+  List.fold_left (fold_trees f) init hs
 
-let iter q f = iter_ph f q
+let fold q ~init f = match q with
+  | None -> init
+  | Some t -> fold_trees f init t
 
+let rec map_trees f (Heap(priority, x, hs)) =
+  Heap(priority, f x, List.map (map_trees f) hs)
 
-let rec fold q ~init f = match q with
-  | Empty -> init
-  | Heap(_, x, hs) ->
-     let init = f init x in
-     List.fold_left (fun init h -> fold h ~init f) init hs
-
-let rec map q f = match q with
-  | Empty -> Empty
-  | Heap(priority, x, hs) ->
-     Heap(priority, f x, List.map (fun h -> map h f) hs)
+let map q f = match q with
+  | None -> None
+  | Some t -> Some(map_trees f t)
