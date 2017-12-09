677d58324a5127808f5433836feb2e7b0d0e9802
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2017-12-09 19:53:27 +0100

    Adapt tests to the new interface

--- tests/test_clip.ml
+++ tests/test_clip.ml
@@ -4,6 +4,6 @@ let () =
   let b = Box2.v (P2.v 0. 0.) (Size2.v 1. 1.) in
   let t = Curve_sampling.of_path
             [(0., -0.5); (1.5, 1.); (0.2, 0.5); (0.3, 1.5); (1., 0.6)] in
-  Curve_sampling.to_file "test_clip0.dat" t;
+  Curve_sampling.to_file t "test_clip0.dat";
   let t1 = Curve_sampling.clip t b in
-  Curve_sampling.to_file "test_clip1.dat" t1
+  Curve_sampling.to_file t1 "test_clip1.dat"
