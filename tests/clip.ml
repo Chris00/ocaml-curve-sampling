53b0f99917719f417ac9d9f0a4e57b84e7fd43c0
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2017-12-12 14:00:55 +0100

    Test clip: add a segment completely outside the box

--- tests/clip.ml
+++ tests/clip.ml
@@ -4,7 +4,7 @@ let () =
   let b = Box2.v (P2.v 0. 0.) (Size2.v 1. 1.) in
   let t = Curve_sampling.of_path
             [(0., -0.5); (1.5, 1.); (0.2, 0.5); (0.3, 1.5); (1., 0.6);
-             (nan, nan); (-1., 0.); (0.5, 0.5)] in
+             (nan, nan); (-0.5, 0.5); (-1., 0.); (0.5, 0.5)] in
   Curve_sampling.to_file t "clip0.dat";
   Curve_sampling.to_latex t "clip0.tex";
   let t1 = Curve_sampling.clip t b in
